7e900652d60ae22b382552bc1a23a6c8cec981c8
Jenae Janzen <63259035+Jjanzen27@users.noreply.github.com>
2021-11-11 10:54:20 -0500

    add analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,27 +21,37 @@ const paragraphStyles = {
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Gatsby Site</title>
-      <h1 style={headingStyles}>
-        💿 Welcome 💿
-        <span style={headingAccentStyles}></span>
-        <span role="img" aria-label="Party popper emojis">
-          It's a beautiful day ⛵️
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        This page is going to be used for A/B testing
-        <span role="img" aria-label="Sunglasses smiley emoji">
-        🎷🎷🎷🎷🎷
-        </span>
-      </p>
-      <h1>This is <em style={headingAccentStyles}>version A</em></h1>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    <html>
+      <head>
+        <script>
+          !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+          posthog.init('phc_g8lqhzHhTBuTA66b3Za1MkXOp0qcU0CDztjF4h43BlK',{api_host:'https://app.posthog.com'})
+        </script>
+      </head>
+      <body>
+        <main style={pageStyles}>
+          <title>Gatsby Site</title>
+          <h1 style={headingStyles}>
+            💿 Welcome 💿
+            <span style={headingAccentStyles}></span>
+            <span role="img" aria-label="Party popper emojis">
+              It's a beautiful day ⛵️
+            </span>
+          </h1>
+          <p style={paragraphStyles}>
+            This page is going to be used for A/B testing
+            <span role="img" aria-label="Sunglasses smiley emoji">
+            🎷🎷🎷🎷🎷
+            </span>
+          </p>
+          <h1>This is <em style={headingAccentStyles}>version A</em></h1>
+          <img
+            alt="Gatsby G Logo"
+            src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+          />
+        </main>
+      </body>
+    </html>
   )
 }
 
